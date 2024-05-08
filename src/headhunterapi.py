91e1a3481c87699f0a58aa4cce6927afa0ed1bda
Chumak Daniil <danilchumak80@gmail.com>
2024-05-08 21:53:39 +0500

    write code inw headhunterapi.py

---
+++ src/headhunterapi.py
@@ -0,0 +1,43 @@
+import requests
+from abc import ABC, abstractmethod
+
+from src.vacancy import Vacancy
+
+
+class Parcer(ABC):
+    """Абстрактный Класс для работы с API HeadHunter"""
+
+    @abstractmethod
+    def load_vacancies(self, profession):
+        """Абстрактный метод для получения и сохранения данных с сайта"""
+        pass
+
+
+class HeadHunterAPI(Parcer):
+    """Класс для работы с API HeadHunter по указанной вакансии."""
+
+    def __init__(self) -> None:
+        self.url: str = 'https://api.hh.ru/vacancies'
+        self.headers: dict = {'User-Agent': 'HH-User-Agent'}
+        self.params: dict = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies: list = []
+        super().__init__()
+
+    def load_vacancies(self, keyword: str) -> list:
+        """Метод для получения данных с сайта """
+        self.params['text'] = keyword
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
+        return [
+            Vacancy(id=info['id'],
+                    name=info['name'],
+                    link_to_vacancy=info['alternate_url'],
+                    salary_from=(info.get('salary', {}) or {}).get('from', 0),
+                    salary_to=(info.get('salary', {}) or {}).get('to', 0),
+                    requirements=info['snippet']['requirement'],
+                    published_at=info['published_at'])
+            for info in response.json()['items']
+        ]
